a1259c9531d5306dfed76a1b1849a21d7d0318f7
Shubhashis Roy <shubhashisroy360@gmail.com>
2026-01-14 22:40:41 +0530

    rag adv init

---
+++ 06-rag-queue/server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Query
+from .queue.connection import queue
+from .queue.worker import process_query
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "Server is up and running"}
+
+@app.post("/chat")
+def chat(
+        query: str = Query(..., description="Chat Message")
+):
+   # Query ko Queue mei daal do
+    job = queue.enqueue(process_query, query)  # process_query(query)
+
+    # User ko bolo your job received
+    return {"status": "queued", "job_id": job.id}
